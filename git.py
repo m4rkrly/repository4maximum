7cf8538c85486855084c798fc21f3de54670003f
mark <m4rkrly@users.noreply.github.com>
2023-08-17 19:08:21 +0700

    Домашка 2:4 + 2:5: HTML и CSS

--- git.py
+++
@@ -1,9 +0,0 @@
-def check_palindrom(word):
-    if word[::-1] == word:
-        return True
-    else:
-        return False
-    
-print(check_palindrom('лепсспел'))
-print('-' * 5)
-print(check_palindrom('helloworld'))
